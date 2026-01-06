0484812b5dc4e8ee73e05a8e03ad6b3b02bd0d27
Jza500 <kittiphob_i@cmu.ac.th>
2026-01-06 16:20:03 +0700

    Donlog 1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,44 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double x[],int y,double z[]){
+    double sum;
+    for (int i = 0; i < y; i++)
+    {
+        sum += x[i];
+    }
+    z[0]= sum / y;
+    double xMean2;
+    for (int i = 0; i < y; i++)
+    {
+        xMean2 += pow(x[i]-z[0],2);
+    }
+    z[1]= sqrt(xMean2/y);
+    double Mult = 1;
+    for (int i = 0; i < y; i++)
+    {
+        Mult *= x[i];
+    }
+    z[2]= pow(Mult,1.0/y);
+    double sumSideDown;
+    for (int i = 0; i < y; i++)
+    {
+        sumSideDown += 1.0/x[i];
+    }
+    z[3]= y/sumSideDown;
+    double Max,Min = x[0];
+    for (int i = 1; i < y; i++)
+    {
+        if (x[i] > Max)
+        {
+            Max = x[i];
+        }
+        if (Min > x[i])
+        {
+            Min = x[i];
+        }
+    }
+    z[4]= Max;
+    z[5]= Min;
+}
